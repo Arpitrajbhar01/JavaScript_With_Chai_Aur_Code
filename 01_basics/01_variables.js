8bc7529b806e6b856a5bb5c5874bba722ce2cf02
Arpitrajbhar01 <rajbhararpit398@gmail.com>
2026-07-29 00:03:40 +0530

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144
+let accountEmail = "Rajbhararpit398@gmail.com"
+var accountPassword = "Aa1234"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc.hc@gmail.com"
+accountPassword = "asdfg"
+accountCity = "Bareilly"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+console.log(accountState)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// number => 2 to power 53
+// bigInt
+// string
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof(undefined)) // undefined
+console.log(typeof(null)) // object
