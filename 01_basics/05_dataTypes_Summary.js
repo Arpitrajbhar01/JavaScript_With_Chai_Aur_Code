27d831a077d117ea63aad8c4c989379a3ef3f708
Arpitrajbhar01 <rajbhararpit398@gmail.com>
2026-07-30 11:43:07 +0530

    Datatype Summary

---
+++ 01_basics/05_dataTypes_Summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 Types : String , Number , Boolean , null , Undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.5
+
+const isLOggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+ 
+const bigNumber = 345675453454345564n
+// const bigNumber = 345675453454345564
+console.log(typeof bigNumber);
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+// Array
+
+const heroes = ["shaktimaan", "naagraj", "doga"]
+
+let myObj = {
+    name: "Arpit"
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
