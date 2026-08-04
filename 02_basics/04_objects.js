1c27bf649741ef7d9c0de21c10a267ba4c3b46a8
Arpitrajbhar01 <rajbhararpit398@gmail.com>
2026-08-04 19:37:27 +0530

    objects in JavaScript is done.

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,7 +17,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
@@ -32,21 +32,42 @@ const obj3 = {...obj1, ...obj2}
 const users = [
     {id: 1,
         email: "a@gmail.com"
-    }
+    },
     {id: 1,
         email: "a@gmail.com"
-    }
+    },
     {id: 1,
         email: "a@gmail.com"
     }
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Arpit"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+//  JSON
+
+// {
+//     "name" : "Arpit",
+//     "cousrename" : "js in hindi",
+//     "price" : "999"
+// }
