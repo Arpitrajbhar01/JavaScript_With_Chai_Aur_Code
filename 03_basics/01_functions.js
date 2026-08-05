e78e8624a308dc1fd7b63abc99004a7c9544c1ab
Arpitrajbhar01 <rajbhararpit398@gmail.com>
2026-08-06 04:05:00 +0530

    Functions with objects and Array.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,7 +11,7 @@ function sayMyName(){
 // sayMyName()
 
 function addTwoNumbers(number1, number2) { // here number1 and number2 is parameter
-    console.log(number1 + number2);
+    // console.log(number1 + number2);
     const sum = number1 + number2
     return (sum)
 }
@@ -34,4 +34,36 @@ function loginUserMessage(username = "Sam") {
 // let printMsg = loginUserMessage("Arpit")
 // console.log(printMsg);
 
-console.log(loginUserMessage("Arpit"));
+// console.log(loginUserMessage("Arpit"));
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(2, 5, 8));
+
+const user = {
+    username: "Arpit",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "Appu",
+    price: 299
+})
+
+const myNewArray = [200, 400, 500, 700, 800]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 1000, 750]));
+
