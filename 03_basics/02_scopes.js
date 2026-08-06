6511709b04da993634a8ee08ee132a55f7c4d3fc
Arpitrajbhar01 <rajbhararpit398@gmail.com>
2026-08-06 20:24:05 +0530

    more addons on scopes in JavaScript.

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,54 @@ if (true) {
     
     let a = 10
     const b =20
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "Arpit"
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username);
+    
+    }
+    // console.log(website);
+    
+    two()
+
+}
+
+one()
+
+if (true) {
+    const username = "Arpit"
+    if (username === "Arpit"){
+        const website = "Youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username)
+
+// +++++++++++++++++++++ interesting ++++++++++++++++++++++
+
+
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
